efec4433600a0811d481acb658dda7cc4fffd39c
William Niemiec <williamniemiec@hotmail.com>
2021-05-17 11:17:46 -0300

    Doc

--- include/pokemaze/engine/Collisions.hpp
+++ include/pokemaze/engine/Collisions.hpp
@@ -2,16 +2,51 @@
 #include <glm/vec4.hpp>
 #include "pokemaze/SceneObject.hpp"
 
+/**
+ * Responsible for collision tests.
+ *
+ * @see     https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
+ */
 class Collisions
 {
+//-------------------------------------------------------------------------
+//		Constructor
+//-------------------------------------------------------------------------
 private:
     Collisions();
 
 
+//-------------------------------------------------------------------------
+//		Methods
+//-------------------------------------------------------------------------
 public:
+    /**
+     * Checks if there is a collision between a point and a plane (AABB).
+     *
+     * @param       point Point coordinates
+     * @param       plane Plane object
+     *
+     * @return      True if there is a collision; false otherwise
+     */
     static bool has_collision_point_plane(glm::vec4 point, SceneObject plane);
-    static bool has_collision_sphere_plane(SceneObject sphere, SceneObject aabb);
-    static bool has_collision_plane_plane(SceneObject obj1, SceneObject obj2);
-    static bool has_collision_point_plane_2d(glm::vec4 point, SceneObject plane);
-    //static double is_vector_less_than_or_equal_to(glm::vec3 left, glm::vec3 right);
+
+    /**
+     * Checks if there is a collision between a sphere and a plane (AABB).
+     * 
+     * @param       sphere Sphere object
+     * @param       plane Plane object
+     *
+     * @return      True if there is a collision; false otherwise
+     */
+    static bool has_collision_sphere_plane(SceneObject sphere, SceneObject plane);
+
+    /**
+     * Checks if there is a collision between two planes (AABB).
+     * 
+     * @param       plane1 Plane object
+     * @param       plane2 Another plane object
+     *
+     * @return      True if there is a collision; false otherwise
+     */
+    static bool has_collision_plane_plane(SceneObject plane1, SceneObject plane2);
 };
--- src/pokemaze/engine/Collisions.cpp
+++ src/pokemaze/engine/Collisions.cpp
@@ -1,119 +1,98 @@
 #include <cmath>
 #include "pokemaze/engine/Collisions.hpp"
 
-//teste cubo-cubo
-//um teste cubo-plano
-//e um teste ponto-esfera
-
-// FONTE: https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
-// teste plano-plano (paredes e jogador)
+//-------------------------------------------------------------------------
+//		Methods
+//-------------------------------------------------------------------------
+// SOURCE: https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
 bool Collisions::has_collision_plane_plane(SceneObject obj1, SceneObject obj2)
 {
-    return  (obj1.bbox_min_world.x <= obj2.bbox_max_world.x && obj1.bbox_max_world.x >= obj2.bbox_min_world.x) &&
-            (obj1.bbox_min_world.y <= obj2.bbox_max_world.y && obj1.bbox_max_world.y >= obj2.bbox_min_world.y) &&
-            (obj1.bbox_min_world.z <= obj2.bbox_max_world.z && obj1.bbox_max_world.z >= obj2.bbox_min_world.z);
+    return  (obj1.bbox_min_world.x <= obj2.bbox_max_world.x && obj1.bbox_max_world.x >= obj2.bbox_min_world.x)
+            && (obj1.bbox_min_world.y <= obj2.bbox_max_world.y && obj1.bbox_max_world.y >= obj2.bbox_min_world.y)
+            && (obj1.bbox_min_world.z <= obj2.bbox_max_world.z && obj1.bbox_max_world.z >= obj2.bbox_min_world.z);
 }
 
-// teste ponto-plano (camera livre e paredes)
+// SOURCE: https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
 bool Collisions::has_collision_point_plane(glm::vec4 point, SceneObject plane)
 {
-    //std::cout << "1: " <<  point.x << " " << plane.bbox_min_world.x << " && " << point.x << " " << plane.bbox_max_world.x << " x: " << (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) << std::endl;
-    //std::cout << "2: " << point.y << " " << plane.bbox_min_world.y << " && " << point.y << " " << plane.bbox_max_world.y << " y: " << (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) << std::endl;
-    //std::cout << "3: " << point.z << " " << plane.bbox_min_world.z << " && " << point.z << " " << plane.bbox_max_world.z << " z: " << (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z) << std::endl;
+    bool has_colision = false;
 
     if (plane.bbox_min_world.x == plane.bbox_max_world.x)
     {
         if (plane.bbox_min_world.x >= 0)
         {
-            return  (point.x >= plane.bbox_min_world.x) &&
-                (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) &&
-                (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
+            has_colision =  (point.x >= plane.bbox_min_world.x)
+                    && (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y)
+                    && (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
         }
         else
         {
-            return  (point.x <= plane.bbox_min_world.x) &&
-                (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) &&
-                (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
+            has_colision =  (point.x <= plane.bbox_min_world.x)
+                    && (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y)
+                    && (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
         }
-
     }
-
-    if (plane.bbox_min_world.y == plane.bbox_max_world.y)
+    else if (plane.bbox_min_world.y == plane.bbox_max_world.y)
     {
         if (plane.bbox_min_world.y >= 0)
         {
-            return  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) &&
-                (point.y >= plane.bbox_min_world.y) &&
-                (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
+            has_colision =  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x)
+                    && (point.y >= plane.bbox_min_world.y)
+                    && (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
         }
         else
         {
-            return  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) &&
-                (point.y <= plane.bbox_min_world.y) &&
-                (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
+            has_colision =  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x)
+                    && (point.y <= plane.bbox_min_world.y) 
+                    && (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
         }
-
-
     }
-
-    if (plane.bbox_min_world.z == plane.bbox_max_world.z)
+    else if (plane.bbox_min_world.z == plane.bbox_max_world.z)
     {
         if (plane.bbox_min_world.z >= 0)
         {
-            return  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) &&
-                (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) &&
-                (point.z >= plane.bbox_min_world.z);
+            has_colision =  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x)
+                    && (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y)
+                    && (point.z >= plane.bbox_min_world.z);
         }
         else
         {
-            return  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) &&
-                (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) &&
-                (point.z <= plane.bbox_min_world.z);
+            has_colision =  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x)
+                    && (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y)
+                    && (point.z <= plane.bbox_min_world.z);
         }
-
-
+    }
+    else
+    {
+        has_colision = (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x)
+                && (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y)
+                && (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
     }
 
-    return  (point.x >= plane.bbox_min_world.x && point.x <= plane.bbox_max_world.x) &&
-            (point.y >= plane.bbox_min_world.y && point.y <= plane.bbox_max_world.y) &&
-            (point.z >= plane.bbox_min_world.z && point.z <= plane.bbox_max_world.z);
+    return  has_colision;
 }
 
-// teste esfera-plano (pokebola e jogador)
+// SOURCE: https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
 bool Collisions::has_collision_sphere_plane(SceneObject sphere, SceneObject aabb)
 {
-    float aabbMinX;
-    float aabbMinY;
-    float aabbMinZ;
-    float aabbMaxX;
-    float aabbMaxY;
-    float aabbMaxZ;
+    double sphere_radius = 1.0;
+    
+    float aabb_min_x = aabb.bbox_min_world.x;
+    float aabb_min_y = aabb.bbox_min_world.y;
+    float aabb_min_z = aabb.bbox_min_world.z;
+    float aabb_max_x = aabb.bbox_max_world.x;
+    float aabb_max_y = aabb.bbox_max_world.y;
+    float aabb_max_z = aabb.bbox_max_world.z;
 
-    aabbMinX = aabb.bbox_min_world.x;
-    aabbMinY = aabb.bbox_min_world.y;
-    aabbMinZ = aabb.bbox_min_world.z;
-    aabbMaxX = aabb.bbox_max_world.x;
-    aabbMaxY = aabb.bbox_max_world.y;
-    aabbMaxZ = aabb.bbox_max_world.z;
+    double x = std::max(aabb_min_x, std::min(sphere.pos.x, aabb_max_x));
+    double y = std::max(aabb_min_y, std::min(sphere.pos.y, aabb_max_y));
+    double z = std::max(aabb_min_z, std::min(sphere.pos.z, aabb_max_z));
 
-    // get box closest point to sphere center by clamping
-    double x = std::max(aabbMinX, std::min(sphere.pos.x, aabbMaxX));
-    double y = std::max(aabbMinY, std::min(sphere.pos.y, aabbMaxY));
-    double z = std::max(aabbMinZ, std::min(sphere.pos.z, aabbMaxZ));
-
-    // this is the same as isPointInsideSphere
-    double distance = sqrt((x - sphere.pos.x) * (x - sphere.pos.x) +
-                             (y - sphere.pos.y) * (y - sphere.pos.y) +
-                             (z - sphere.pos.z) * (z - sphere.pos.z));
-    double sphereRadius = 1.0;
-
-    return distance < sphereRadius;
+    double distance = sqrt(
+            (x - sphere.pos.x) * (x - sphere.pos.x)
+            + (y - sphere.pos.y) * (y - sphere.pos.y)
+            + (z - sphere.pos.z) * (z - sphere.pos.z)
+    );
+    
+    return distance < sphere_radius;
 }
-/*
-double Collisions::is_vector_less_than_or_equal_to(glm::vec3 left, glm::vec3 right)
-{
-    return  left.x <= right.x
-            && left.y <= right.y
-            && left.z <= right.z;
-}*/
-
